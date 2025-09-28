2679a06218702ea6b5ef39203d064bd055f278e6
asiangoldfish <duongkhai@gmail.com>
2025-09-29 00:30:45 +0200

    refactor: create application class and introduce argparser

---
+++ gtt.py
@@ -0,0 +1,42 @@
+import argparse
+from src import Application
+from sys import argv
+
+VERSION = '1.0'
+
+parser = argparse.ArgumentParser(
+    description='The GitLab time tracker analytics tool')
+
+# Flags
+parser.add_argument(
+    "--format",
+    help="Choose output format: table, json"
+)
+
+# Options
+parser.add_argument('--version', action='store_true',
+                    help='print the current version')
+parser.add_argument('--debug', action='store_true', help='enable debug mode')
+
+args = parser.parse_args()
+
+# Show help page. At least one argument must be present
+if len(argv) == 1:
+    parser.print_help()
+    exit(0)
+
+if args.version:
+    print(f"GTT version {VERSION}")
+    exit(0)
+
+app = Application()
+app.debug = args.debug
+
+if not app.set_format(args.format):
+    print(f"'{args.format}' is not a valid format. Supported formats:")
+    for format in app.allowed_formats:
+        print(f'\t{format}')
+    exit(1)
+
+if not app.init():
+    exit(1)
