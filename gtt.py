8ea96e0f380d62a2c35d50b99f835934ec5caa23
Khai Duong <duongkhai@gmail.com>
2025-09-29 14:52:19 +0200

    feat: measure time estimation deviation for a snigle user

--- gtt.py
+++ gtt.py
@@ -7,17 +7,19 @@ VERSION = '1.0'
 parser = argparse.ArgumentParser(
     description='The GitLab time tracker analytics tool')
 
-# Flags
+# Command
 parser.add_argument(
     "--format",
     help="Choose output format: table, json"
 )
+parser.add_argument('--time-deviation', action='store_true', help='Show time deviation for a user')
 
 # Options
 parser.add_argument('--version', action='store_true',
                     help='print the current version')
 parser.add_argument('--debug', action='store_true', help='enable debug mode')
 parser.add_argument('--sync', action='store_true', help='synchronise with remote')
+parser.add_argument('--user')
 
 args = parser.parse_args()
 
@@ -41,3 +43,5 @@ if args.sync and app.sync():
 
 if args.format:
     app.show(args.format)
+elif args.time_deviation:
+    app.show_time_estimate_deviation(args.user)
--- src/application.py
+++ src/application.py
@@ -95,7 +95,7 @@ class Application:
 
         for issue in issues:
             stats = issue["time_stats"]
-            
+
             data.append({
                 "issue_id": issue["iid"],
                 "title": issue["title"],
@@ -106,3 +106,56 @@ class Application:
 
         df = pd.DataFrame(data)
         df.to_csv("data/gitlab_time_tracking.csv", index=False)
+
+    def show_time_estimate_deviation(self, user: str) -> None:
+        """
+        For a specific user, compute the time estimate deviation.
+
+        Arguments
+        ---------
+        user:
+            Full name of the user in question.
+        """
+        if not user or len(user) == 0:
+            print("Missing user name")
+            return
+
+        if not path.exists('data/gitlab_response.json'):
+            if not self.sync():
+                print('Failed to synchronise with remote')
+                return
+
+        # Gather all issues related to the user
+        data = []
+        issues = {}
+        with open('data/gitlab_response.json') as fp:
+            issues = json.load(fp)
+
+        for issue in issues:
+            if not issue["assignee"] or issue["assignee"]["name"] != user:
+                continue
+
+            stats = issue["time_stats"]
+
+            data.append({
+                "estimate": stats["time_estimate"],
+                "spent": stats["total_time_spent"],
+            })
+
+        if len(data) == 0:
+            print("No user was found")
+            return
+
+        # Compute the deviation
+        total_estimated_time = 0    # in seconds
+        total_spent_time = 0        # in seconds
+        for d in data:
+            total_estimated_time += d.get('estimate')
+            total_spent_time += d.get('spent')
+
+        print(f'Total estimated: {total_estimated_time}s')
+        print(f'Total spent: {total_spent_time}s')
+
+        print(f'Accuracy: {total_spent_time*100//total_estimated_time}%')
+        print(f'Intepretation: <100% means spent less than estimated time. \
+>100% means spent more than estimated time.')
